bd23a51d05348defa719a512d4284e47968812e1
MicrQ <mail.micrq@proton.me>
2024-06-29 13:16:49 +0300

    EDITED: created Promise object

--- 0x01-ES6_promise/0-promise.js
+++ 0x01-ES6_promise/0-promise.js
@@ -1,8 +1,9 @@
 export default function getResponseFromAPI() {
   return new Promise((resolve, reject) => {
-    if (1)
+    if (1) {
       resolve();
-    else
+    } else {
       reject();
+    }
   });
 }
