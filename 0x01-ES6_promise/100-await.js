c6fe3bfa51f11b65275fe79096b9b719189dc610
MicrQ <mail.micrq@proton.me>
2024-06-29 20:14:55 +0300

    more try catch with await

---
+++ 0x01-ES6_promise/100-await.js
@@ -0,0 +1,15 @@
+import { createUser, uploadPhoto } from './utils';
+
+export default async function asyncUploadUser() {
+  let obj = {};
+
+  try {
+    const photo = await uploadPhoto();
+    const user = await createUser();
+    obj = {photo, user};
+  } catch(e) {
+    obj = {photo: null, user: null};
+  }
+
+  return obj;
+}
