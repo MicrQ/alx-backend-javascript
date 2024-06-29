2005def39fc051a9f744c8d5660dd6be581b57a9
MicrQ <mail.micrq@proton.me>
2024-06-29 13:14:55 +0300

    no unused variable

--- 0x01-ES6_promise/0-promise.js
+++ 0x01-ES6_promise/0-promise.js
@@ -1,3 +1,8 @@
 export default function getResponseFromAPI() {
-  return new Promise((resolve, reject) => {});
+  return new Promise((resolve, reject) => {
+    if (1)
+      resolve();
+    else
+      reject();
+  });
 }
