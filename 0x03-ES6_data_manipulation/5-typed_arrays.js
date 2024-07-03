5c4ae29153cd5f74d59c77580d3391c5efd6ca8f
MicrQ <mail.micrq@proton.me>
2024-07-03 07:37:52 +0300

    used dataview constractor

--- 0x03-ES6_data_manipulation/5-typed_arrays.js
+++ 0x03-ES6_data_manipulation/5-typed_arrays.js
@@ -1,10 +1,11 @@
 export default function createInt8TypedArray(length, position, value) {
+  if (position >= length || position < 0) {
+    throw Error('Position outside range');
+  }
   const buffer = new ArrayBuffer(length);
+  const dataView = new DataView(buffer, 0, length);
   const int8Array = new Int8Array(buffer);
 
-  if (position >= length || position < 0) {
-    throw new Error('Position outside range');
-  }
   int8Array[position] = value;
-  return buffer;
+  return dataView;
 }
