428e8a0cade7e2a59eb7bf4423fcbe70c528f68f
MicrQ <mail.micrq@proton.me>
2024-07-03 07:19:29 +0300

    no-param assignment. created a new object instead

--- 0x03-ES6_data_manipulation/4-update_grade_by_city.js
+++ 0x03-ES6_data_manipulation/4-update_grade_by_city.js
@@ -2,7 +2,9 @@ export default function updateStudentGradeByCity(students, city, grades) {
   return students.filter((student) => student.location === city)
     .map((student) => {
       const gradeObj = grades.find((grade) => grade.studentId === student.id);
-      student.grade = gradeObj ? gradeObj.grade : 'N/A';
-      return student;
+      return {
+        ...student,
+        grade: gradeObj ? gradeObj.grade : 'N/A',
+      };
     });
 }
