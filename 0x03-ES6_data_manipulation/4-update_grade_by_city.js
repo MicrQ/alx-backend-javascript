a49e7aaa96a77b086c3a9095b1fba2178279509b
MicrQ <mail.micrq@proton.me>
2024-07-03 07:15:22 +0300

    indentation corrected, single quote used

--- 0x03-ES6_data_manipulation/4-update_grade_by_city.js
+++ 0x03-ES6_data_manipulation/4-update_grade_by_city.js
@@ -2,7 +2,7 @@ export default function updateStudentGradeByCity(students, city, grades) {
   return students.filter((student) => student.location === city)
     .map((student) => {
       const gradeObj = grades.find((grade) => grade.studentId === student.id);
-      student.grade = gradeObj ? gradeObj.grade : "N/A";
+      student.grade = gradeObj ? gradeObj.grade : 'N/A';
       return student;
     });
 }
