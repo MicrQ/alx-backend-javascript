773da68e99747fd70ed1e03462e8cf7a0bc86dd3
MicrQ <mail.micrq@proton.me>
2024-07-03 17:47:35 +0300

    LINT: changed the approach

--- 0x03-ES6_data_manipulation/100-weak.js
+++ 0x03-ES6_data_manipulation/100-weak.js
@@ -2,10 +2,10 @@ export const weakMap = new WeakMap();
 
 export function queryAPI(endpoint) {
   let value = weakMap.get(endpoint) || 0;
-  value++;
+  value += 1;
 
   if (value >= 5) {
-    throw Error('Endpoint load is high');
+    throw new Error('Endpoint load is high');
   } else {
     weakMap.set(endpoint, value);
   }
