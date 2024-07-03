7e6746f54deb8760446d687f0e66595aef3f5d36
MicrQ <mail.micrq@proton.me>
2024-07-03 07:12:32 +0300

    indentation corrected

--- 0x03-ES6_data_manipulation/4-update_grade_by_city.js
+++ 0x03-ES6_data_manipulation/4-update_grade_by_city.js
@@ -1,8 +1,8 @@
 export default function updateStudentGradeByCity(students, city, grades) {
   return students.filter((student) => student.location === city)
-  .map((student) => {
-    const gradeObj = grades.find((grade) => grade.studentId === student.id);
-    student.grade = gradeObj ? gradeObj.grade : "N/A";
-    return student;
-  });
+    .map((student) => {
+      const gradeObj = grades.find((grade) => grade.studentId === student.id);
+      student.grade = gradeObj ? gradeObj.grade : "N/A";
+      return student;
+    });
 }
