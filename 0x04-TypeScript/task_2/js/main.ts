1e7a03b6efeb24e8658012a6a3bf3a87a8a29424
MicrQ <mail.micrq@proton.me>
2024-07-05 23:24:27 +0300

    created a functions for the classes

--- 0x04-TypeScript/task_2/js/main.ts
+++ 0x04-TypeScript/task_2/js/main.ts
@@ -46,3 +46,15 @@ function createEmployee(salary: number | string): Director | Teacher {
     return new Director();
   }
 }
+
+function isDirector(employee: Director | Teacher): boolean {
+  return employee instanceof Director;
+}
+
+function executeWork(employee: Director | Teacher): string {
+  if (employee instanceof Director) {
+    return employee.workDirectorTasks();
+  } else {
+    return employee.workTeacherTasks();
+  }
+}
