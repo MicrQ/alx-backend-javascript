c5a5172e9c93b1bbd0080638e6af6fa7a5da0ef8
AbenetGebre <mail.micrq@proton.me>
2024-08-24 23:42:49 +0300

    camel cased snake cases

--- 0x05-Node_JS_basic/2-read_file.js
+++ 0x05-Node_JS_basic/2-read_file.js
@@ -12,27 +12,27 @@ const countStudents = (db_path) => {
     .toString('utf-8')
     .trim()
     .split('\n');
-  const stud_grp = {};
-  const field_names = fileLines[0].split(',');
-  const studentPropNames = field_names.slice(0, field_names.length - 1);
+  const studGrp = {};
+  const fieldNames = fileLines[0].split(',');
+  const studentPropNames = fieldNames.slice(0, fieldNames.length - 1);
 
   for (const line of fileLines.slice(1)) {
     const studentRecord = line.split(',');
     const studentPropValues = studentRecord.slice(0, studentRecord.length - 1);
     const field = studentRecord[studentRecord.length - 1];
-    if (!Object.keys(stud_grp).includes(field)) {
-      stud_grp[field] = [];
+    if (!Object.keys(studGrp).includes(field)) {
+      studGrp[field] = [];
     }
     const studentEntries = studentPropNames
       .map((propName, idx) => [propName, studentPropValues[idx]]);
-    stud_grp[field].push(Object.fromEntries(studentEntries));
+    studGrp[field].push(Object.fromEntries(studentEntries));
   }
 
   const totalStudents = Object
-    .values(stud_grp)
+    .values(studGrp)
     .reduce((pre, cur) => (pre || []).length + cur.length);
   console.log(`Number of students: ${totalStudents}`);
-  for (const [field, group] of Object.entries(stud_grp)) {
+  for (const [field, group] of Object.entries(studGrp)) {
     const studentNames = group.map((student) => student.firstname).join(', ');
     console.log(`Number of students in ${field}: ${group.length}. List: ${studentNames}`);
   }
